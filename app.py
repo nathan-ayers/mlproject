bc597260a1e4a24524a77c958ec29963834875c8
Nathan Ayers <nathanayers17@gmail.com>
2025-01-28 19:27:15 -0500

    started creating app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import pickle
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+## route for a home page
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    
+    else:
+        data=C
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -110,9 +110,6 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
-
-
 
             
         except Exception as e:
